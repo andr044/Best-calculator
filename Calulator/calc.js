01a1adbb35b216c1e1e2eb97888b871553ef9a71
Andr <smirnov.ad1993.js@gmail.ru>
2020-08-21 18:44:25 +0300

    21.08

--- Calulator/calc.js
+++ Calulator/calc.js
@@ -29,3 +29,12 @@ function ymn() {
         result = num1*num2;
             document.getElementById('out').innerHTML = result;
 }
+function razd() {
+    var num1, num2, result;
+    num1 = document.getElementById('n1').value;
+    num1 = parseInt(num1);
+        num2 = document.getElementById('n2').value;
+        num2 = parseInt(num2);
+        result = num1/num2;
+            document.getElementById('out').innerHTML = result;
+}
